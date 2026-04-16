4263219916775eb5bcb0d471212bccc14e9228c8
anushridudhe75-rgb <anushridudhe75@gmail.com>
2026-04-17 00:03:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,23 +15,20 @@ app.use(express.json());
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-// API Routes
-app.use("/api/auth", require("./routes/authRoutes"));
+// API routes
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
 app.use("/api/wishlist", require("./routes/wishlistRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 
-// Test Route
+// test route
 app.get("/test", (req, res) => {
   res.send("Server working");
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-
-// Server Start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// ✅ FIX (ADD THIS LAST)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
