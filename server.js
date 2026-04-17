70034c681df024cda8fff9671120e2956c7d523a
anushridudhe75-rgb <anushridudhe75@gmail.com>
2026-04-17 09:16:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,13 @@ app.get("/test", (req, res) => {
   res.send("Server working");
 });
 
-// ✅ FINAL FIX (VERY IMPORTANT)
+// Server frontend
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+// START SERVER 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
